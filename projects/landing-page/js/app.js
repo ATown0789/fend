0cce24d587a0b03f8fe852eb0d53fa41b2bb0096
Anthony Collini <collini.anthony@gmail.com>
2020-06-30 10:20:07 -0500

    feat: Dynamic building of navbar

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -18,12 +18,22 @@
  * 
 */
 
-
+const sectionList = document.querySelectorAll('section');
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+
+function isNearTop(section) {
+  const sectionSize = section.getBoundingClientRect();
+  const html = document.documentElement;
+  return (
+    sectionSize.top >= 0 &&
+	sectionSize.top <= 100 &&
+    sectionSize.bottom <= (window.innerHeight || html.clientHeight))
+  );
+}
 
 
 
@@ -34,9 +44,24 @@
 */
 
 // build the nav
-
+function navBuild() {
+	const navFrag = document.createDocumentFragment();
+	for(let i = 0; i < sectionList.length; i++){
+		const newA = document.createElement('a');
+		const newLi = document.createElement('li');
+		const elId = sectionList[i].id;
+		newA.setAttribute('class', 'menu__link');
+		newA.setAttribute('href','#'+ elId)
+		newLi.textContent = elId;
+		newA.appendChild(newLi);
+		navFrag.appendChild(newA);
+	}
+	
+	document.getElementById('navbar__list').appendChild(navFrag);
+}
 
 // Add class 'active' to section when near top of viewport
+
 
 
 // Scroll to anchor ID using scrollTO event
@@ -49,6 +74,7 @@
 */
 
 // Build menu 
+navBuild();
 
 // Scroll to section on link click
 
